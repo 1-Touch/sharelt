bb4edf93b8c1f48b2dc168a6fac249ae34064ec2
Deepak-S1ngh <deepak@1touch-dev.com>
2020-07-16 19:50:10 +0530

    folder structure

--- server/controllers/scrapers/insta-scraper.js
+++ server/controllers/scrapers/insta-scraper.js
@@ -7,3 +7,13 @@ const people = (req, res) => {
 };
 
 module.exports = { person, people };
+
+// const exec = require('child_process').exec;
+// exec('instaloader profile espnmma',['-ltr'],(e, stdout,stderr) => {
+//     if (e instanceof Error) {
+//         console.error(e);
+//         throw e;
+//     }
+//     console.log('stdout',stdout);
+//     console.log('stderr',stderr);
+// });
