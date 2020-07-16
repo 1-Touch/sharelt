0868364e3af3ec6a3b987abdeb89abdf8925cf31
ashish1touch <ashish@1touch.market>
2020-07-16 13:11:12 +0530

    server setup

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv/config');
+
+// Constants
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => res.send('in app'));
+// app.use('/facebook', require('./routes/facebook'));
+
+// PORT
+const PORT = 5000 || process.env.PORT;
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
