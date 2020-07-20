83c0288b307014e05a89321230b5124aa89433d1
Deepak-S1ngh <deepak@1touch-dev.com>
2020-07-20 15:17:36 +0530

    Fixed the folder structure and added Twitter-scraper file

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-require('dotenv/config');
+require("dotenv/config");
 
 // Constants
 const app = express();
@@ -11,9 +11,10 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.get('/', (req, res) => res.send('in app'));
-app.use('/facebook', require('./routes/facebook/pages'));
-app.use('/insta-scraper', require('./routes/scrapers/insta-scraper'));
+app.get("/", (req, res) => res.send("in app"));
+app.use("/facebook", require("./routes/facebook/pages"));
+app.use("/insta-scraper", require("./routes/instagram/insta-scraper"));
+app.use("/twitter-scraper", require("./routes/twitter/twitter-scraper"));
 
 // PORT
 const PORT = 5000 || process.env.PORT;
